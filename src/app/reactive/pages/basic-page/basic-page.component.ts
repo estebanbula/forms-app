0c32f3c5ebba6bf3fd7f37535ad743d560c726e3
Carlos Esteban Bula <carlos.bula@insoftar.com>
2024-01-08 18:42:15 -0500

    Basic and dynamic forms

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   templateUrl: './basic-page.component.html',
-  styles: [
-  ]
+  styles: []
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit {
 
+  ngOnInit(): void {
+    this.myBasicForm.reset({
+      name: '',
+      price: 0,
+      stock: 0
+    })
+  }
+
+  public myBasicForm: FormGroup = this.formBuilder.group({
+    name: ['', [Validators.required, Validators.minLength(3)], []],
+    price: [0, [Validators.required, Validators.minLength(0)], []],
+    stock: [0, [Validators.required, Validators.minLength(0)], []],
+  });
+
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  public onSave(): void {
+    if (this.myBasicForm.invalid) return;
+
+    console.log(this.myBasicForm.value);
+    this.myBasicForm.reset({price: 0, stock: 0});
+  }
+
+  public isValidField(field: string): boolean | null {
+    return this.myBasicForm.controls[field].errors &&
+      this.myBasicForm.controls[field].touched;
+  }
+
+  public getFieldError(field: string): string | null {
+    if (!this.myBasicForm.controls[field]) return null;
+
+    const errors = this.myBasicForm.controls[field].errors || {};
+
+    for (let error of Object.keys(errors)) {
+      switch (error) {
+        case 'required':
+          return 'This field is required.';
+        case 'minlength':
+          return `This field must have at least ${errors['minlength'].requiredLength} characters.`;
+      }
+    }
+
+    return null;
+  }
 }
--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormArray, FormBuilder, FormControl, FormGroup, ValidationErrors, Validators} from "@angular/forms";
 
 @Component({
   templateUrl: './dynamic-page.component.html',
@@ -6,5 +7,70 @@ import { Component } from '@angular/core';
   ]
 })
 export class DynamicPageComponent {
+
+  public myDynamicForm: FormGroup = this.formBuilder.group({
+    name: ['', [Validators.required, Validators.minLength(3)], []],
+    favoriteGames: this.formBuilder.array([]),
+  });
+
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  public onSave(): void {
+    if (this.myDynamicForm.invalid) {
+      this.myDynamicForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myDynamicForm.value);
+    (this.myDynamicForm.controls['favoriteGames'] as FormArray) = this.formBuilder.array([]);
+    this.myDynamicForm.reset();
+  }
+
+  get favoriteGames() {
+    return this.myDynamicForm.get('favoriteGames') as FormArray;
+  }
+
+  public newFavorite: FormControl = new FormControl('', Validators.required);
+
+  public isValidField(field: string): boolean | null {
+    return this.myDynamicForm.controls[field].errors &&
+      this.myDynamicForm.controls[field].touched;
+  }
+
+  public isValidFieldArray(formArray: FormArray, index: number): boolean | null {
+    return formArray.controls[index].invalid &&
+      formArray.controls[index].touched;
+  }
+
+  public getFieldError(field: string): string | null {
+    if (!this.myDynamicForm.controls[field]) return null;
+
+    const errors: ValidationErrors = this.myDynamicForm.controls[field].errors || {};
+
+    for (let error of Object.keys(errors)) {
+      switch (error) {
+        case 'required':
+          return 'This field is required.';
+        case 'minlength':
+          return `This field must have at least ${errors['minlength'].requiredLength} characters.`;
+      }
+    }
+
+    return null;
+  }
+
+  public onDeleteFavorite(index: number): void {
+    this.favoriteGames.removeAt(index);
+  }
+
+  public onAddFavorite(): void {
+    if (this.myDynamicForm.invalid) return;
+    const newGame = this.newFavorite.value;
+    this.favoriteGames.push(
+      this.formBuilder.control(newGame, Validators.required)
+    )
+    this.newFavorite.reset();
+  }
 
 }
