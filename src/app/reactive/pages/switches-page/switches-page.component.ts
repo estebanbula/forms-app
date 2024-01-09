5977ece0f086fb70cd32725c1956b12bc1669ab8
Carlos Esteban Bula <carlos.bula@insoftar.com>
2024-01-09 11:50:48 -0500

    Form switches

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+
+interface Person {
+  gender: string,
+  wantNotifications: boolean
+}
 
 @Component({
   templateUrl: './switches-page.component.html',
-  styles: [
-  ]
+  styles: []
 })
-export class SwitchesPageComponent {
+export class SwitchesPageComponent implements OnInit {
+
+  public mySwitchForm: FormGroup = this.formBuilder.group({
+    gender: ['M', Validators.required],
+    wantNotifications: [true, Validators.required],
+    termsAndConditions: [false, Validators.requiredTrue],
+  })
+
+  public person: Person = {
+    gender: 'M',
+    wantNotifications: false
+  }
+
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    this.mySwitchForm.reset(this.person);
+  }
+
+  public isValidField(field: string): boolean | null {
+    return this.mySwitchForm.controls[field].errors &&
+      this.mySwitchForm.controls[field].touched;
+  }
+
+  public onSave(): void {
+    if (this.mySwitchForm.invalid) {
+      this.mySwitchForm.markAllAsTouched();
+      return;
+    }
+
+    const {termsAndConditions, ...newPerson} = this.mySwitchForm.value;
+
+    this.person = newPerson;
+    console.log(this.person)
+    console.log(this.mySwitchForm.value)
+  }
 
 }
